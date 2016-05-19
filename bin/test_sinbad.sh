7226215f86f4777f792c1177b2a8606f871393e3
krish <krish.vasudevan@polestarglobal.com>
2016-05-19 22:18:03 +0100

    run test_sinbad.sh

--- bin/test_sinbad.sh
+++ bin/test_sinbad.sh
@@ -10,6 +10,7 @@ cd accent/accent
 ./build
 
 export ACCENT_DIR=${HOME}/accent
-
 echo $ACCENT_DIR
-#python ../src/sinbad -b dynamic1 -d 10 amb2.acc general.lex
+
+git clone git@github.com:nvasudevan/sinbad.git ~/sinbad
+python ~/sinbad/src/sinbad -b dynamic1 -d 10 amb2.acc general.lex
