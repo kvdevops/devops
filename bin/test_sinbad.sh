d995d6978dc5df9ed41efa308e67091fe5d2b333
krish <krish.vasudevan@polestarglobal.com>
2016-05-19 23:25:52 +0100

    stay in current directory

--- bin/test_sinbad.sh
+++ bin/test_sinbad.sh
@@ -2,17 +2,15 @@
 
 # run sinbad against a example grammar
 
+currdir=$(pwd)
+echo $currdir
 # build ACCENT
-cd ~
-wget http://accent.compilertools.net/accent.tar
-tar xf accent.tar
-cd accent/accent
-./build
-
-echo $?
+wget http://accent.compilertools.net/accent.tar -O ~/accent.tar
+tar xf ~/accent.tar -C ~/
+(cd ~/accent/accent && ./build)
 
 export ACCENT_DIR=${HOME}/accent
 echo $ACCENT_DIR
 
 git clone https://github.com/nvasudevan/sinbad.git ~/sinbad
-python ~/sinbad/src/sinbad -b dynamic1 -d 10 amb2.acc general.lex
+python ~/sinbad/src/sinbad -b dynamic1 -d 10 bin/amb2.acc bin/general.lex
