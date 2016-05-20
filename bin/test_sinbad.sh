78de8b12c0828c4e6279752f3c3b549df5f389d4
krish <krish.vasudevan@polestarglobal.com>
2016-05-20 10:39:06 +0100

    patch entire

--- bin/test_sinbad.sh
+++ bin/test_sinbad.sh
@@ -9,6 +9,10 @@ wget http://accent.compilertools.net/accent.tar -O ~/accent.tar
 tar xf ~/accent.tar -C ~/
 (cd ~/accent/accent && ./build)
 
+# apply patch for entire -- essentially sets up an exit code
+wget https://raw.githubusercontent.com/nvasudevan/experiment/master/patches/entire.c.patch -O ~/entire.c.patch
+patch -b -p0 ~/accent/entire/entire.c < ~/entire.c.patch
+
 export ACCENT_DIR=${HOME}/accent
 echo $ACCENT_DIR
 
