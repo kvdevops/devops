19d5ae34205c90ed064e560f01d99123b46acff6
krish <krish.vasudevan@polestarglobal.com>
2016-05-20 11:24:17 +0100

    Split out the accent build stage from the test stage

--- bin/test_sinbad.sh
+++ bin/test_sinbad.sh
@@ -2,19 +2,7 @@
 
 # run sinbad against a example grammar
 
-currdir=$(pwd)
-echo $currdir
-# build ACCENT
-wget http://accent.compilertools.net/accent.tar -O ~/accent.tar
-tar xf ~/accent.tar -C ~/
-(cd ~/accent/accent && ./build)
-
-# apply patch for entire -- essentially sets up an exit code
-wget https://raw.githubusercontent.com/nvasudevan/experiment/master/patches/entire.c.patch -O ~/entire.c.patch
-patch -b -p0 ~/accent/entire/entire.c < ~/entire.c.patch
-
-export ACCENT_DIR=${HOME}/accent
-echo $ACCENT_DIR
-
 git clone https://github.com/nvasudevan/sinbad.git ~/sinbad
 python ~/sinbad/src/sinbad -b dynamic1 -d 10 bin/amb2.acc bin/general.lex
+python ~/sinbad/src/sinbad -b dynamic3 -d 10 bin/amb2.acc bin/general.lex
+python ~/sinbad/src/sinbad -b dynamic4 -d 10 -w 0.1 bin/amb2.acc bin/general.lex
